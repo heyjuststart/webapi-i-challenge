c0228612cace1e068b52f6b7be4058ccdb97fa2d
heyjuststart <41658450+heyjuststart@users.noreply.github.com>
2019-03-18 14:04:04 -0400

    add cors, use cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 // implement your API here
 const express = require('express');
 const User = require('./data/db');
+const cors = require('cors');
 
 const server = express();
 server.use(express.json());
 server.use(express.urlencoded({ extended: false }));
+server.use(cors());
 
 server.get('/', (req, res) => {
   res.send('Hello World');
